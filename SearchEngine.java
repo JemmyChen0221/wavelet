f2e894078a19d7882b0d8e840e0904a4f5635e69
JemmyChen0221 <mic018@ucsd.edu>
2022-10-13 19:34:32 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -6,48 +6,37 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-
+    ArrayList<String> list = new ArrayList<String>();
     public String handleRequest(URI url) {
-        ArrayList<String> list = new ArrayList<String>();
+        
         if (url.getPath().contains("/add")) {
-            
-
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
                 list.add(parameters[1]);
-                return list.get(0);
-
+                return "added";
+            } else{
+                return "not added";
             }
-            return "Not Added";
-
-        }else if(url.getPath().contains("/show")){
-            String word = "";
-            return word;
-            /*for(int i = 0; i < list.size();i ++){
-                word = word + "," + list.get(i);
-            }
-            return word;*/
-
         }
         else {
             if (url.getPath().contains("/search")) {
+                String output = "";
                 String[] para = url.getQuery().split("=");
                 if (para[0].equals("s")) {
                     String check = para[1];
-                    String output = "";
                     for(int i = 0; i < list.size(); i++){
                         if(list.get(i).contains(check)){
-                            return "found";
+                            output = output + ", " + list.get(i);
                         }
                     }
 
                     
                     //return String.valueOf(count);
                 }
+                return output.substring(1);
             } else{
                 return "Try to Add or Search Something!";
             }
-            return "Welcome";
             
         }
     }
